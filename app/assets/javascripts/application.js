93a0851044af49bc67ae39df6322c9f68cc0acf1
Raghu Betina <rbetina@gmail.com>
2013-06-10 11:56:27 -0500

    Added draggable/droppable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require private_pub
 //= require_tree .
@@ -9,3 +10,38 @@
 //     alert("Yo!");
 //   });
 // });
+
+$(document).ready(function() {
+  $(".note").draggable();
+
+  $( "#trash" ).droppable({
+        drop: function( event, ui ) {
+          $(ui.draggable).fadeOut();
+          $.ajax({
+            url: "/notes/" + ui.draggable.data('nid'),
+            type: 'delete'
+          });
+        }
+      });
+});
+
+
+// $.ajax({
+//   url: "http://fiddle.jshell.net/favicon.png",
+//   beforeSend: function ( xhr ) {
+//     xhr.overrideMimeType("text/plain; charset=x-user-defined");
+//   }
+// }).done(function ( data ) {
+//   if( console && console.log ) {
+//     console.log("Sample of data:", data.slice(0, 100));
+//   }
+// });
+
+
+
+
+
+
+
+
+
